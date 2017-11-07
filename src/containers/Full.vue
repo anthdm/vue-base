c25b6779c44ab2ed4126c8c9fe94a3af6549f853
anthdm <anthony@sparse.ai>
2017-11-07 21:00:40 +0100

    created Full container for holding base app html structure

---
+++ src/containers/Full.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app-header"> 
+    <AppHeader/>
+    <div class="app-body">
+      <div class="container-fluid">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader'
+
+export default {
+  name: 'full',
+  components: {
+    AppHeader
+  }
+}
+</script>
